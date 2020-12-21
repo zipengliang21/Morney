4530f8d6535cb855a94d885ef066a3fd93e6b51e
Zipeng(Jepson) <zipengliang21@gmail.com>
2020-12-21 12:07:15 -0800

    Add Icons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require('icons/dollar.svg');
+require('icons/tag.svg');
+require('icons/chart.svg');
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -10,7 +13,14 @@ const NavWrapper = styled.nav`
     > li{
     width: 33.3%;
     text-align: center;
-    padding: 16px;
+    padding: 4px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .icon {
+      width: 24px;
+      height: 24px;
+    }
     }
   }
 `;
@@ -20,12 +30,21 @@ const Nav = () => {
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#tag"/>
+                    </svg>
                     <Link to="tags">Tag Page</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#dollar"/>
+                    </svg>
                     <Link to="/money">Money</Link>
                 </li>
                 <li>
+                    <svg  className="icon">
+                        <use xlinkHref="#chart"/>
+                    </svg>
                     <Link to="/statistics">Statistics Report</Link>
                 </li>
             </ul>
