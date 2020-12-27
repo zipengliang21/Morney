e045e6eb87095d8d6ed60e9a197d9fdeb811a97c
Zipeng(Jepson) <zipengliang21@gmail.com>
2020-12-26 20:12:25 -0800

    Create Button Component

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,22 +1,11 @@
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler, useRef} from 'react';
+import {Input} from 'components/Input';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
   padding: 0 16px;
   font-size: 14px;
-  > label {
-    display:flex;
-    align-items: center;
-    > span { margin-right: 16px; white-space: nowrap; }
-    > input {
-      display:block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
 `;
 
 type Props = {
@@ -26,21 +15,12 @@ type Props = {
 
 const NoteSection: React.FC<Props> = (props) => {
     const note = props.value;
-    const refInput = useRef<HTMLInputElement>(null);
-    const onBlur = () => {
-        if (refInput.current !== null) {
-            props.onChange(refInput.current.value);
-        }
+    const onChange:ChangeEventHandler<HTMLInputElement> = (e) => {
+        props.onChange(e.target.value);
     };
     return (
         <Wrapper>
-            <label>
-                <span>Note:</span>
-                <input type="text" placeholder="Add Your Note in Here"
-                       defaultValue={note}
-                       onBlur={onBlur}
-                />
-            </label>
+            <Input label="Note" type="text" value={note} onChange={onChange}/>
         </Wrapper>
     );
 };
