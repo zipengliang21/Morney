a054fe6915dc124c68344e2888d15cc29571002e
Zipeng(Jepson) <zipengliang21@gmail.com>
2020-12-27 10:50:16 -0800

    Refactor updateTag and deleteTag in useTags.tsx

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -10,32 +10,25 @@ const defaultTags = [
 
 const useTags = () => { // Create a custom React Hook
     const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
-    const findTag = (id: number) => {return tags.filter(tag => tag.id === id)[0];}
+    const findTag = (id: number) => {
+        return tags.filter(tag => tag.id === id)[0];
+    };
     const findTagIndex = (id: number) => {
         let result = -1;
-        for(let i =0; i< tags.length; i++) {
-            if(tags[i].id === id ) {
+        for (let i = 0; i < tags.length; i++) {
+            if (tags[i].id === id) {
                 result = i;
                 break;
             }
         }
         return result;
     };
-    const updateTag = (id: number, obj: {name:string}) => {
-        // Obtain the index of the Tag you want to update
-        const index = findTagIndex(id);
-        // clone the original tags into the new one
-        const tagsClone = JSON.parse(JSON.stringify(tags));
-        // replace the new tag
-        tagsClone.splice(index, 1, {id: id, name: obj.name});
-        setTags(tagsClone);
+    const updateTag = (id: number, {name}: { name: string }) => {
+        setTags(tags.map(tag => tag.id === id ? {id, name} : tag));
     };
     const deleteTag = (id: number) => {
-        const index = findTagIndex(id);
-        const tagsClone = JSON.parse(JSON.stringify(tags));
-        tagsClone.splice(index, 1);
-        setTags(tagsClone);
-    }
+        setTags(tags.filter(tag => tag.id !== id));
+    };
     return {tags, setTags, findTag, updateTag, findTagIndex, deleteTag};
 };
 
